18537f3c52443bb6b807aa3b7a12e7186ee277ab
Brahimovith <brahim.sabri.contact@gmail.com>
2024-06-10 00:19:23 +0000

    upp

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,16 @@
-#!/usr/bin/python3
-
-
-def canUnlockAll
+#!/usr/bin/python
+def canUnlockAll(boxes):
+    s = []
+    for i in boxes:
+        for j in i:
+            s.append(j)
+    
+    l = set(s)
+    print(l)
+    for i in range(1,len(boxes)):
+        if(i in l):
+            continue
+        return False
+    return True
         
 
